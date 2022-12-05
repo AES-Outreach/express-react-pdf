38328a8312b0d88cd020074d27002416f81a5e7f
Ivana Erlich <ivanabe3@gmail.com>
2022-12-05 11:27:23 -0500

    cleanup

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,5 +1,3 @@
 export { pdfMiddlewareBuilder } from "./builder/pdfMiddlewareBuilder";
-
 export { basePDFGenerator } from "./middleware/basePDFGenerator";
-
 export { BaseRootContext } from "./rootContexts/baseRootContext";
